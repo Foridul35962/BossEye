03562df678a71b9894d5b56de14bcae14ce222e2
Foridul35962 <foridulislam35962@gmail.com>
2026-07-04 11:45:53 +0600

    add delete data from database

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 import Devices from "../model/Devices.model.js";
 import deviceData from "../db/devices.json" assert { type: "json" };
+import PowerHistory from "../model/PowerHistory.model.js";
+import Alerts from "../model/Alert.model.js";
 
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URL}/bosseye`);
         console.log("Database connected");
+
+        // PowerHistory.deleteMany()
+        // Alerts.deleteMany()
 
         const count = await Devices.countDocuments();
 
